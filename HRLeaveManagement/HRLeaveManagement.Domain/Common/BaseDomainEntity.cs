b7c1e1969c868cd3c9be1cfe8cb52b46e4931587
ghulam murtaza <asif.raza@bytedash.my>
2024-04-18 10:49:03 +0500

    Inital project setup and added domain Entities

---
+++ HRLeaveManagement/HRLeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagement.Domain.Common
+{
+    public class BaseDomainEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime LastModifiedDate { get; set; }  
+
+        public string LastModifiedBy { get; set; }
+    }
+}
