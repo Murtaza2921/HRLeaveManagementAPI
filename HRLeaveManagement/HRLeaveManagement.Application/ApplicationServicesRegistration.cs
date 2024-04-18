53853c97fe6d9f21c0d67f6aea63f5818f353691
ghulam murtaza <asif.raza@bytedash.my>
2024-04-18 11:50:06 +0500

    added DTOs, added and configure AutoMapper and added a services registration file to register the dependancies and service

---
+++ HRLeaveManagement/HRLeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace HRLeaveManagement.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static void CongfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
